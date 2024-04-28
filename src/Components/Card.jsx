3d4857186dfe0667bf874466dba05c69314f204f
doeun7879 <168330232+doeun7879@users.noreply.github.com>
2024-04-28 14:27:09 +0700

    Add files via upload

---
+++ src/Components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Card.css"
+
+const Card = () => {
+  return (
+    <>
+    <div className='cd'>This is Card Components</div>
+    <button className='bt1'>Click Here</button>
+    <button className='bt2'>Click Me</button>
+    </>
+  );
+};
+
+export default Card
